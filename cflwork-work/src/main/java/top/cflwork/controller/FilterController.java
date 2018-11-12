7e6b3bdbb114c0bfb73eece23b48c35b3e523850
NDCFL <275300091@qq.com>
2018-11-12 10:21:47 +0800

    修改springmvc的过滤拦截

--- cflwork-work/src/main/java/top/cflwork/controller/FilterController.java
+++ cflwork-work/src/main/java/top/cflwork/controller/FilterController.java
@@ -1,13 +1,26 @@
 package top.cflwork.controller;
 
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import top.cflwork.vo.UserVo;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class FilterController implements HandlerInterceptor {
+
+    private List<String> exceptUrls;
+
+    public List<String> getExceptUrls() {
+        return exceptUrls;
+    }
+
+    public void setExceptUrls(List<String> exceptUrls) {
+        this.exceptUrls = exceptUrls;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -18,72 +31,42 @@ public class FilterController implements HandlerInterceptor {
         response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("XDomainRequestAllowed", "1");
-        Object user = request.getSession().getAttribute("userVo");
-        String path = request.getRequestURL().toString();
-        if (user == null) {
-            if(path.contains("/index") ||
-                    path.contains("user/loginPage") ||
-                    path.contains("user/getInfo") ||
-                    path.contains("static") ||
-                    path.contains("captcha") ||
-                    path.contains("/logins") ||
-                    path.contains("house/contract")||
-                    path.contains("contractMaster") ||
-                    path.contains("file")||
-                    path.contains("upload")){
+        String requestUri = request.getRequestURI();
+        if (requestUri.startsWith(request.getContextPath())) {
+            requestUri = requestUri.substring(request.getContextPath().length(), requestUri.length());
+        }
+        //系统根目录
+        if (StringUtils.equals("/", requestUri)) {
+            return true;
+        }
+        //放行exceptUrls中配置的url
+        for (String url : exceptUrls ) {
+            if (url.endsWith("/**")) {
+                if (requestUri.startsWith(url.substring(0, url.length() - 3))) {
+                    return true;
+                }
+            } else if (requestUri.startsWith(url)) {
                 return true;
             }
+        }
+        UserVo userVo = (UserVo)request.getSession().getAttribute("userVo");
+        //如果没有登录
+        if (null == userVo) {
             System.out.println("尚未登录，调到登录页面");
             response.sendRedirect("/user/loginPage");
             return false;
+        } else {
+            return true;
         }
-        return true;
     }
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle");
-        Object user = request.getSession().getAttribute("userVo");
-        String path = request.getRequestURL().toString();
-        if (user == null) {
-            if(path.contains("/index") ||
-                    path.contains("user/loginPage") ||
-                    path.contains("user/getInfo") ||
-                    path.contains("static") ||
-                    path.contains("captcha") ||
-                    path.contains("/logins") ||
-                    path.contains("house/contract")||
-                    path.contains("contractMaster") ||
-                    path.contains("file")||
-                    path.contains("upload")){
 
-            }else{
-                System.out.println("尚未登录，调到登录页面");
-                request.getRequestDispatcher("/user/loginPage").forward(request,response);
-            }
-        }
     }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion");
-        Object user = request.getSession().getAttribute("userVo");
-        String path = request.getRequestURL().toString();
-        if (user == null) {
-            if(
-                    path.contains("/index") ||
-                    path.contains("user/loginPage") ||
-                    path.contains("user/getInfo") ||
-                    path.contains("static") ||
-                    path.contains("captcha") ||
-                    path.contains("/logins") ||
-                    path.contains("house/contract")||
-                    path.contains("contractMaster") ||
-                    path.contains("file")||
-                    path.contains("upload")){
 
-            }else{
-                System.out.println("尚未登录，调到登录页面");
-                request.getRequestDispatcher("/user/loginPage").forward(request,response);
-            }
-        }
     }
 }
