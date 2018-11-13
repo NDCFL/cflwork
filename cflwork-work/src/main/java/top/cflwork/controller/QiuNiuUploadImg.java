0712305fdbc05ea5a338c154ff0f2bbef829eab1
NDCFL <275300091@qq.com>
2018-11-13 11:04:54 +0800

    修改基本参数

--- cflwork-work/src/main/java/top/cflwork/controller/QiuNiuUploadImg.java
+++ cflwork-work/src/main/java/top/cflwork/controller/QiuNiuUploadImg.java
@@ -6,6 +6,9 @@ import java.io.File;
 
 public class QiuNiuUploadImg {
     public static void main(String[] args) {
-        System.out.println(QiniuUtil.commonUploadFileForKey(new File("F:\\web\\hotel\\images\\weixin.png"),"test"));
+        //删除文件
+        System.out.println(QiniuUtil.deleteFile("upload/faceImg/2018/1113/2bc03d09497c4077.jpg"));
+        //测试上传本地文件
+//        System.out.println(QiniuUtil.commonUploadFileForKey(new File("F:\\web\\hotel\\images\\weixin.png"),"test"));
     }
 }
